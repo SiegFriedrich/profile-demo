cbbb63c3e15706ea16bb9b8c06a0f2c9d0fe1df5
Sieg Friedrich <siegfriedrich@outlook.com>
2023-05-17 01:37:11 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style lang="scss" scoped>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+
+
+html,
+body,
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100%;
+  width: 100%;
+}
+
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-main class="main"><router-view></router-view></el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.header {
+  position: inherit;
+  height: 80px;
+  background-color: burlywood;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+}
+
+.side {
+  background-color: orange;
+  height: 100vh;
+}
+
+.main {
+  background-color: yellow;
+  height: 100vh;
+}
+</style>
